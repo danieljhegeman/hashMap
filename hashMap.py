c9ff511b3932150d7f1dc523f5e7e4fe5aa3ed4c
Daniel J Hegeman <danieljhegeman@gmail.com>
2018-06-07 22:25:48 -0700

    Init

---
+++ hashMap.py
@@ -0,0 +1,12 @@
+class HashMap():
+  def __init__(self, size=4):
+
+  def getIndex(self, key):
+
+  def insert(self, key, val):
+
+  def retrieve(self, key):
+
+  def remove(self, key):
+  
+  def resize(self):
